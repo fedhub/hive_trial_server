54fc673725725309f0ae793897e280b931712c55
shimonsw <shimi0007@hotmail.com>
2015-08-25 13:41:33 +0300

    WIP

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var app        = express();
 var server     = require('http').createServer(app);
 var path       = require('path');
 var bodyParser = require('body-parser');
+var Parse      = require('parse').Parse;
+Parse.initialize("YmjyJjNGA1QcCYQQ0C479gArZzWKBEgcwBe3kt7K", "NRJGn0tlpDY2By3mRPhlGcDFb0dclEQedm993YjV");
+var query = new Parse.Query(Parse.User);
+query.find({
+    success: function(users) {
+        for (var i = 0; i < users.length; ++i) {
+            console.log(users[i].get('username'));
+        }
+    }
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +30,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use(require('./queries'));
+//app.use(require('./queries'));
 
 var port = process.env.PORT || 3000;
 server.listen(port, function(){
